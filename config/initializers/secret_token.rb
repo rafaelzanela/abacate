311012fe44031b16b3bcca53940c547d313ad60e
Rodrigo Hassel <rodri.hassel@gmail.com>
2013-07-11 17:20:53 -0300

    Criado um projeto em branco usando o active admin, postgresql e configurado para o banco execute em localhost.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Abacate::Application.config.secret_token = '7a18fa7791a8c9970842d169be4250749eea370a67da50e374a5c21a922a9458007ee6fb7c54ac37100d2efb569cebd90f693ee0f5d83eb3ddd3a01f38176f14'
